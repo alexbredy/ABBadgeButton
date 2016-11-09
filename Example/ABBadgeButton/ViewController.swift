71d7e414055d7085e5ac1d09b9308a0b75fb348f
Alexander Bredy <alex@goodwall.org>
2016-11-09 18:49:46 +0100

    Updates comments in example project

--- Example/ABBadgeButton/ViewController.swift
+++ Example/ABBadgeButton/ViewController.swift
@@ -20,15 +20,20 @@ class ViewController: UIViewController {
 
         title = "BADGE BUTTON DEMO"
         
+        /*Basic usage of the button - gets displayed by setting the badgeValue property*/
         textButton.badgeValue = "1"
         textButton.badgeBackgroundColor = UIColor.blue
         
+        /*  Works with icon fonts as well. In this example we are also overriding the badge origin offset in order to position the badge where we want
+         */
         chatButton.titleLabel?.font = UIFont(name: "IcoMoon-Free", size: 30)
         chatButton.setTitle("\u{e96d}", for: .normal)
         chatButton.badgeValue = "5"
         chatButton.setTitleColor(UIColor.white, for: .normal)
         chatButton.badgeOriginOffset = CGPoint(x: -5, y: 5)
     
+        /* This example illustrates setting the badge button when an image is used for the button instead of text. We are also setting a border width and a border color.
+         */
         imageButton.badgeValue = "10"
         imageButton.badgeBorderColor = UIColor.white.cgColor
         imageButton.badgeOriginOffset = CGPoint(x: 0, y: 12)
@@ -36,6 +41,8 @@ class ViewController: UIViewController {
         imageButton.badgeBorderWidth = 2
         imageButton.badgeBackgroundColor = UIColor.purple
         
+        /* This last example showcases the usage of ABBadgeButton inside a UINavigationBar bar button
+         */
         let smileyButton = ABBadgeButton(type: .system)
         smileyButton.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
         smileyButton.titleLabel?.font = UIFont(name: "IcoMoon-Free", size: 24)
