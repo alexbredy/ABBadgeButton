81de34fd186d6b75db485c48d10482de278b7cd8
Alexander Bredy <alex@goodwall.org>
2016-11-09 18:12:54 +0100

    Updates badge offset default value

--- Example/ABBadgeButton/ViewController.swift
+++ Example/ABBadgeButton/ViewController.swift
@@ -11,12 +11,13 @@ import ABBadgeButton
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var chatButton: ABBadgeButton!
     @IBOutlet weak var imageButton: ABBadgeButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationBar.isUserInteractionEnabled = false
         // Do any additional setup after loading the view, typically from a nib.
         chatButton.titleLabel?.font = UIFont(name: "IcoMoon-Free", size: 30)
         chatButton.setTitle("\u{e96d}", for: .normal)
@@ -29,11 +30,21 @@ class ViewController: UIViewController {
         imageButton.badgeFont = UIFont.systemFont(ofSize: 20)
         imageButton.badgeBorderWidth = 2
         imageButton.badgeBackgroundColor = UIColor.purple
+        
+        let smileyButton = ABBadgeButton(frame: CGRect(x: 0, y: 0, width: 24, height: 24))
+        smileyButton.titleLabel?.font = UIFont(name: "IcoMoon-Free", size: 24)
+        smileyButton.setTitle("\u{e9e9}", for: .normal)
+        smileyButton.badgeOriginOffset = CGPoint(x: 0, y: 0)
+        smileyButton.badgeValue = "9"
+        smileyButton.addTarget(self, action: #selector(didSelectBarButton), for: .touchUpInside)
+        
+        let barButton = UIBarButtonItem(customView: smileyButton)
+        
+        navigationItem.rightBarButtonItem = barButton
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func didSelectBarButton(){
+        print("button clicked")
     }
 
 }
